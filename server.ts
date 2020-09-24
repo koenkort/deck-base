5963394a55b2696a913208b270372c6470f4af5a
koenkort <koenkortw@gmail.com>
2020-09-24 15:04:50 +0200

    Setup back-end mongoDB

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express = require('express')
+import cors = require('cors');
+import mongoose = require('mongoose');
+import * as http from 'http';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('<h1>Banaan</h1>')
+})
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+server.listen(PORT);
+server.on("listening", () => {
+    console.info(`Server is running on http://locahost:${PORT}`);
+})

dcf06b10a6af82a192d0a954e6596a394df07ea6
koenkort <koenkortw@gmail.com>
2020-10-12 15:02:20 +0200

    Made database connection

--- server.ts
+++ server.ts
@@ -8,9 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const db_url = "mongodb+srv://koen:banaan69@deck-base.8wc6n.mongodb.net/test"
+console.log(db_url);
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('<h1>Hello World!</h1>');
 })
+
+mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8080;
 
